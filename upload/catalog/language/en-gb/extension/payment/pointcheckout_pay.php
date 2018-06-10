530a23f457e63ebf64646d56fb00561275f6229d
yaser safou <yasser@pointcheckout.com>
2018-06-10 11:48:15 +0300

    logo image styling

--- upload/catalog/language/en-gb/extension/payment/pointcheckout_pay.php
+++ upload/catalog/language/en-gb/extension/payment/pointcheckout_pay.php
@@ -1,8 +1,8 @@
 <?php
 // Text
-$_['text_title']			     	 ='<img src="https://www.pointcheckout.com/image/logo.png" />';
+$_['text_title']			     	 ='<img src="https://www.pointcheckout.com/image/logo.png" width="250px" />';
 $_['text_wait']				     = 'Please wait!';
 $_['text_points']			     = 'PointCheckout Points Payment';
-$_['text_pointcheckout_pay']		 = '<img src="https://www.pointcheckout.com/image/logo.png" alt="pointcheckout Website " title="pointcheckout Website Payment " />';
+$_['text_pointcheckout_pay']		 = '<img src="https://www.pointcheckout.com/image/logo.png" width="250px" alt="pointcheckout Website " title="pointcheckout Website Payment " />';
 $_['text_pointcheckout_note']     = 'you will be Redirected to PointCheckout website to complete your payment process';
 
